b8e970e2caca1c946273e9b87b4a07e6127c84ea
Bill Heaton <pixelhandler@gmail.com>
2014-02-01 00:04:30 -0800

    clear out fixtures

--- client/app/fixtures.js
+++ client/app/fixtures.js
@@ -1,15 +1,1 @@
-/*
-App.Slide.FIXTURES = [
-  { id: '0', filename: 'http://fpoimg.com/800x600?text=Title'},
-  { id: '1', filename: 'http://fpoimg.com/800x600?text=Section-A', milliseconds: 1000 },
-  { id: '2', filename: 'http://fpoimg.com/800x600?text=Slide-A1', milliseconds: 500 },
-  { id: '3', filename: 'http://fpoimg.com/800x600?text=Slide-A2', milliseconds: 250 },
-  { id: '4', filename: 'http://fpoimg.com/800x600?text=Slide-A3' },
-  { id: '5', filename: 'http://fpoimg.com/800x600?text=Section-B', milliseconds: 300 },
-  { id: '6', filename: 'http://fpoimg.com/800x600?text=Slide-B1', milliseconds: 300 },
-  { id: '7', filename: 'http://fpoimg.com/800x600?text=Slide-B2', milliseconds: 300 },
-  { id: '8', filename: 'http://fpoimg.com/800x600?text=Slide-B3' },
-  { id: '9', filename: 'http://fpoimg.com/800x600?text=The End'},
-  { id: '10', iframeUrl: 'http://dev.w3.org/html5/markup/iframe.html#iframe'}
-];
-*/
+/* App.Post.FIXTURES = []; */
