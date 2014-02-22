476d84c973c40d7fe8f639a3d59d84b24bdece60
Bill Heaton <pixelhandler@gmail.com>
2014-02-22 10:51:07 -0800

    Update index test to use show more link

--- client/tests/integration/index_test.js
+++ client/tests/integration/index_test.js
@@ -15,16 +15,20 @@ module('Index', {
 var excerpt = '.Blog-content .Blog-excerpt';
 var showMore = '.Blog-content .Blog-showMore';
 
-test('Index template, lists (5) posts with link for more', function () {
-  expect(7);
+test('Index template, lists posts with link to show more', function () {
+  expect(8);
   visit('/').then(function () {
     ok(exists(excerpt), 'Index template has excerpt(s).');
     ok(exists(excerpt + ' .Blog-excerpt-title'), 'excerpt title exists');
     ok(exists(excerpt + ' .Blog-excerpt-title a'), 'excerpt title has anchor');
     ok(exists(excerpt + ' .Blog-excerpt-summary'), 'excerpt summary exists');
     ok(exists(excerpt + ' .u-button'), 'excerpt button exists');
+    equal(find(excerpt).length, 5, 'index page has five (5) post excerpts');
+
     ok(exists(showMore), 'Show more link exists');
-    equal(find(excerpt).length, 5, 'index page has five post excerpts');
+    click(showMore).then(function () {
+      equal(find(excerpt).length, 9, 'nine (9) post excerpts shown after click on show more');
+    });
   });
 });
 
